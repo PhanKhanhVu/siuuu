1cf99f579362d8844e57d2e4b19264f52965a5e3
PhanKhanhVu <hi3sirin@gmail.com>
2025-02-28 11:46:08 +0700

    Add files via upload

---
+++ FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Skidibi Toilet :3");
+        System.exit(0);
+    }
+}
